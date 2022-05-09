f9b31e49922c1bcda8e1b9b8acec4d357b0aef1a
Pablo Gómez <pgomezm0486@alumno.ipn.mx>
2022-05-09 07:18:35 -0500

    Actualizacion en venta

--- components/VentaContainer.js
+++ components/VentaContainer.js
@@ -1,17 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ButtonGroup, Card, Button, ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
+import { ButtonGroup, Card, Button, ToggleButton, ToggleButtonGroup, Form, InputGroup, FormControl } from 'react-bootstrap'
 import CarritoContext from './Carrito'
 import ProductoThumbnail from './ProductoThumbnail'
 
 const VentaContainer = () => {
 
 	const {carrito, setCarrito} = useContext(CarritoContext)
-	const [metodoPago, setMetodoPago] = useState('Efectivo');
+	const [metodoPago, setMetodoPago] = useState('Efectivo')
 	const [total, setTotal] = useState(0)
+	const [monto, setMonto] = useState(0)
 
 	const radios = [
-		{ name: <i className='bi bi-cash-coin fs-1'/>, value: 'Efectivo' },
-		{ name: <i className='bi bi-credit-card-2-front-fill fs-1'/>, value: 'Tarjeta de crédito' },
+		{ name: <i className='bi bi-cash-coin fs-3'/>, value: 'Efectivo' },
+		{ name: <i className='bi bi-credit-card-2-front-fill fs-3'/>, value: 'Tarjeta de crédito' },
 	 ];
 
 	useEffect(() => {
@@ -66,6 +67,18 @@ const VentaContainer = () => {
 					<div className='hstack'>
 						<span>Método de pago:</span>
 						<span className='ms-auto text-truncate text-muted' style={{maxWidth: '140px'}}>{metodoPago}</span>
+					</div>
+					<div className='hstack align-content-center'>
+						<span className=''>Método recibido:</span>
+						<InputGroup size="sm" className="w-50 ms-auto">
+							<InputGroup.Text>$</InputGroup.Text>
+							<FormControl value={monto} type="numeric" className='text-end' placeholder='00' disabled={total > 0 ? false : true} onChange={({currentTarget}) => setMonto(currentTarget.value ? currentTarget.value : 0)}/>
+							<InputGroup.Text>.00</InputGroup.Text>
+						</InputGroup>
+					</div>
+					<div className='hstack align-content-center mt-2'>
+						<span className=''>Cambio:</span>
+						<span className='ms-auto fw-bolder'>${monto - total > 0 ? (monto - total).toFixed(2) : (0).toFixed(2)}</span>
 					</div>
 				</div>
 				<button className='btn btn-lg btn-primary'>
