4e721699199b8beddbf3cc7f551b9727e22f2329
Pablo Gómez <pgomezm0486@alumno.ipn.mx>
2022-05-29 13:03:09 -0500

    Branch clean

--- components/TableHeadCell.js
+++
@@ -1,8 +0,0 @@
-import { TableCell } from '@mui/material'
-import React from 'react'
-
-const TableHeadCell = ({titulo}) => {
-  return <TableCell sx={{ fontWeight: "bolder", textAlign: "center" }} >{titulo}</TableCell>
-}
-
-export default TableHeadCell
